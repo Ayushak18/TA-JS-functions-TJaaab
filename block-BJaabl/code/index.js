09fbeaf11188b0768b5fbb0e6fefa75392ce6fee
Ayush Kamboj <aayushkamboj7@gmail.com>
2020-12-07 17:16:53 +0530

    Function block 4

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,10 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+function sayHello (name){
+  alert(`Hello ${name}`);
+}
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +14,11 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName(firstName, lastName){
+  return `${firstName} ${lastName}`;
+}
+
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -20,6 +29,10 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+
+function addTwoNumbers(firstNum ,secondNum){
+  return firstNum + secondNum;
+}
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,6 +46,18 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc (numA ,numB ,operation){
+  if(operation ==='add'){
+    return numA + numB;
+  }else if(operation ==='sub'){
+    return numA - numB;
+  }else if(operation ==='mul'){
+    return numA * numB;
+  }else if(operation ==='div'){
+    return numA / numB;
+  }
+}
+
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +67,23 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear(number){
+  if(number % 4==0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+
+function getFactorial(number){
+  let factorial = 1;
+  for(let i = 1; i<=number;i++){
+    factorial = factorial * i;
+  }
+  return factorial;
+}
